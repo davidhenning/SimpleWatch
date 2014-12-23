401c740c251bd83d2e82224bc5257b802d91c087
David Henning <madcat.me@gmail.com>
2014-12-23 20:25:26 +0100

    Add date

--- src/main.c
+++ src/main.c
@@ -3,6 +3,7 @@
 static Window *s_main_window;
 
 static TextLayer *s_time_layer;
+static TextLayer *s_date_layer;
 
 static void main_window_load(Window *window) {
   // Create time TextLayer
@@ -10,18 +11,28 @@ static void main_window_load(Window *window) {
   text_layer_set_background_color(s_time_layer, GColorClear);
   text_layer_set_text_color(s_time_layer, GColorBlack);
   text_layer_set_text(s_time_layer, "00:00");
+  
+  s_date_layer = text_layer_create(GRect(0, 105, 144, 30));
+  text_layer_set_background_color(s_date_layer, GColorClear);
+  text_layer_set_text_color(s_date_layer, GColorBlack);
+  text_layer_set_text(s_date_layer, "00.00.0000");
 
   // Improve the layout to be more like a watchface
   text_layer_set_font(s_time_layer, fonts_get_system_font(FONT_KEY_BITHAM_42_BOLD));
   text_layer_set_text_alignment(s_time_layer, GTextAlignmentCenter);
+  
+  text_layer_set_font(s_date_layer, fonts_get_system_font(FONT_KEY_GOTHIC_28_BOLD));
+  text_layer_set_text_alignment(s_date_layer, GTextAlignmentCenter);
 
   // Add it as a child layer to the Window's root layer
   layer_add_child(window_get_root_layer(window), text_layer_get_layer(s_time_layer));
+  layer_add_child(window_get_root_layer(window), text_layer_get_layer(s_date_layer));
 }
 
 static void main_window_unload(Window *window) {
     // Destroy TextLayer
     text_layer_destroy(s_time_layer);
+    text_layer_destroy(s_date_layer);
 }
 
 static void update_time() {
@@ -31,6 +42,7 @@ static void update_time() {
 
   // Create a long-lived buffer
   static char buffer[] = "00:00";
+  static char date_buffer[] = "00.00.0000";
 
   // Write the current hours and minutes into the buffer
   if(clock_is_24h_style() == true) {
@@ -40,9 +52,12 @@ static void update_time() {
     // Use 12 hour format
     strftime(buffer, sizeof("00:00"), "%I:%M", tick_time);
   }
+  
+  strftime(date_buffer, sizeof("00.00.0000"), "%d.%m.%Y", tick_time);
 
   // Display this time on the TextLayer
   text_layer_set_text(s_time_layer, buffer);
+  text_layer_set_text(s_date_layer, date_buffer);
 }
 
 static void tick_handler(struct tm *tick_time, TimeUnits units_changed) {
